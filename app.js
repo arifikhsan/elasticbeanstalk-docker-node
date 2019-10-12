e2c158a2b60d7921371daa4d89961479c28ba68d
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-10-12 20:31:37 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// get dependencies
+var express = require('express');
+var morgan = require('morgan');
+
+var app = express();
+var server = require('http').Server(app);
+port = process.env.PORT || 8080;
+
+// catch all other routes and return the index file
+app.get('/', (_, res) => {
+  res.send("Hello World !!");
+});
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+server.listen(port);
+console.log('App running at http://localhost:' + port);
+
